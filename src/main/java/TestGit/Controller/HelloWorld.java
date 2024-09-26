d0e0e89d4aebbeff50cc655647ac76e2dba8649e
Delvin204 <lonkaka2608@gmail.com>
2024-09-26 20:32:50 +0700

    upload

---
+++ src/main/java/TestGit/Controller/HelloWorld.java
@@ -0,0 +1,7 @@
+package TestGit.Controller;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Chào b Dương");
+	}
+}
---
+++ src/test/java/TestGit/TestGitApplicationTests.java
@@ -0,0 +1,13 @@
+package TestGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
