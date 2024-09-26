0232dec0ccb146dec676c56fb851eedd1d3f9618
Delvin204 <lonkaka2608@gmail.com>
2024-09-26 20:51:57 +0700

    chao Duong

--- src/main/java/TestGit/Controller/HelloWorld.java
+++ src/main/java/TestGit/Controller/HelloWorld.java
@@ -2,6 +2,6 @@ package TestGit.Controller;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Chào b Long");
+		System.out.println("Chào b Duong");
 	}
 }
