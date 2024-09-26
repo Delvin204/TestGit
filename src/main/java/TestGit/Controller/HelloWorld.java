0330be8d77b3165bfd8053ee80b3828b3f194ead
Delvin204 <lonkaka2608@gmail.com>
2024-09-26 21:26:40 +0700

    Chào bạn Dương

--- src/main/java/TestGit/Controller/HelloWorld.java
+++ src/main/java/TestGit/Controller/HelloWorld.java
@@ -2,6 +2,6 @@ package TestGit.Controller;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Chào ban Duong");
+		System.out.println("Chào bạn Dương");
 	}
 }
