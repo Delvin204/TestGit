5e3a76eb473a366d86ecfd42bb14a9d32f010825
D-Ngow <kyucmuathu3@gmail.com>
2024-09-30 19:08:25 +0700

    abcxyz

--- src/main/java/TestGit/Controller/HelloWorld.java
+++ src/main/java/TestGit/Controller/HelloWorld.java
@@ -3,8 +3,6 @@ package TestGit.Controller;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Chào bạn Dương");
-		if(true) {
-			System.out.println("Hello world def1");
-		}
+		System.out.println("abcxyz");
 	}
 }
