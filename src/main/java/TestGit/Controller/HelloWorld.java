766959db88e0b0e7ec3cd45f4c634944fd071ada
Delvin204 <lonkaka2608@gmail.com>
2024-09-30 18:58:57 +0700

    def

--- src/main/java/TestGit/Controller/HelloWorld.java
+++ src/main/java/TestGit/Controller/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Chào bạn Dương");
 		if(true) {
-			System.out.println("Hello world");
+			System.out.println("Hello world def");
 		}
 	}
 }
