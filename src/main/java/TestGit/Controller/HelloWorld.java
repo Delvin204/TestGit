1006f3afcedda6767f41d5678950f61ea977042f
Delvin204 <lonkaka2608@gmail.com>
2024-09-30 19:42:25 +0700

    sửa

--- src/main/java/TestGit/Controller/HelloWorld.java
+++ src/main/java/TestGit/Controller/HelloWorld.java
@@ -2,7 +2,7 @@ package TestGit.Controller;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Chào bạn Dương");ư
+		System.out.println("Chào bạn Dương");
 		System.out.println("abcxyz");
 		if(true) {
 			System.out.println("Hello world def1");
